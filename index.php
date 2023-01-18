8e6be0cf509b0dd490dfc4b6c6b863ac42f4348d
RinoceronteDigital <rinoceronte.digital@gmail.com>
2023-01-18 13:21:39 -0600

    Cambio Home 2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
-    <title>Git 1 - Digital-Age Resistance</title>
+    <title>Git 2 - Digital-Age Resistance</title>
 
     <meta name="robots" content="Follow">
     <meta name="googlebot" content="Follow, Index">
